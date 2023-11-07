e383a842c034aaa5e3f071028db6e8f704a3a036
Erfan Mahram <erfanmahram@gmail.com>
2023-11-08 00:23:53 +0330

    [ADD] Add urls.py for university app [MODIFY] Settings.py and urls and views changed and swagger added to project

--- university/views.py
+++ university/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response("test", status=status.HTTP_200_OK)
