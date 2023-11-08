3ad945e498ef893878e6a3ee70a8d83677662a5d
semjz <sam.malakjan@gmail.com>
2023-11-09 03:05:53 +0330

    [Modify] Change username field to user_id

--- authentication/managers.py
+++ authentication/managers.py
@@ -7,7 +7,7 @@ import string
 
 
 class CustomUserManger(UserManager):
-    def create_user(self, username, email=None, password=None, **extra_fields):
+    def create_user(self, user_id, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         email = self.normalize_email(email)
@@ -15,21 +15,21 @@ class CustomUserManger(UserManager):
         is_staff = extra_fields.get("is_staff")
         if not role and not is_staff:
             raise ValueError("The user role must be set")
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(user_id=user_id, email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
+    def create_superuser(self, user_id, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("role", "It-manager")
-        username = ''.join(secrets.choice(string.digits) for _ in range(4))
+        user_id = ''.join(secrets.choice(string.digits) for _ in range(4))
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(g("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(g("Superuser must have is_superuser=True."))
-        self.create_user(username, email, password, **extra_fields)
-        print(f"user id: {username}")
+        self.create_user(user_id, email, password, **extra_fields)
+        print(f"user id: {user_id}")
