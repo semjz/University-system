e3632479f5421f53c6fb34eb5b244b20143ec9eb
semjz <sam.malakjan@gmail.com>
2023-11-09 16:10:54 +0330

    [Add] Add registration feature

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LogoutView, TestView
+from .views import RegisterView, LogoutView, TestView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 
 app_name = "authentication"
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("login/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,14 @@
-from datetime import datetime, timedelta
-
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+
+User = get_user_model()
 
 
 class LogoutView(APIView):
@@ -20,6 +23,12 @@ class LogoutView(APIView):
 
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
 
 
 class TestView(APIView):
