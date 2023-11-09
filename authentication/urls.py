037203a981c04ab250fc130b1f782d58b8bbfac2
semjz <sam.malakjan@gmail.com>
2023-11-09 21:15:42 +0330

    #36 [Modify] Add Password reset views

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LogoutView, TestView
+from .views import RegisterView, LogoutView, PasswordResetRequest, PasswordResetAction, TestView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -8,6 +8,8 @@ from rest_framework_simplejwt.views import (
 app_name = "authentication"
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("change-password-request/", PasswordResetRequest.as_view(), name="reset-password-request"),
+    path("change-password-action/", PasswordResetAction.as_view(), name="reset-password-action"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("login/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -7,14 +8,19 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
+from django.conf import settings
+from django.core.cache import cache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+import uuid
 
 User = get_user_model()
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
+    def post(self, request:Request):
         try:
             refresh_token = request.data["refresh_token"]
             refresh_token = RefreshToken(refresh_token)
@@ -29,6 +35,33 @@ class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class PasswordResetRequest(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request: Request):
+        reset_token = uuid.uuid4()
+        if not cache.get(request.user.user_id):
+            cache.set(request.user.user_id, reset_token, CACHE_TTL)
+            return Response(f"reset-token: {cache.get(request.user.user_id)}", status=status.HTTP_200_OK)
+        else:
+            return Response("A password reset request is already submitted", status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordResetAction(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request:Request):
+        if cache.get(request.user.user_id):
+            if request.data["reset_token"] == cache.get(request.user.user_id):
+                new_password = request.data["new_password"]
+                request.user.set_password(new_password)
+                return Response("Password successfully changed!", status=status.HTTP_200_OK)
+            else:
+                return Response("Reset token is wrong!", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("No password reset request for this user!", status=status.HTTP_400_BAD_REQUEST)
 
 
 class TestView(APIView):
