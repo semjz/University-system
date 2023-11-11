8d1c11f264fcd5378dbcbaca644157477729e66d
semjz <sam.malakjan@gmail.com>
2023-11-11 20:17:55 +0330

    #35 [Add] Create Course model

--- university/models.py
+++ university/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+
+COURSE_TYPES = [("general", "General"), ("specialized", "Specialized")
+    , ("core", "Core"), ("optional", "Optional")]
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    credits = models.FloatField(blank=False, null=False, validators=(MinValueValidator(0),))
+    type = models.CharField(choices=COURSE_TYPES, blank=False, null=False)
+    pre_requisites = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True)
+    co_requisites = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True)
+    # school = models.ManyToManyField(School)
