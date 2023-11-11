c195f9a6a104ad8fb2f3f95f31599782988b80cf
semjz <sam.malakjan@gmail.com>
2023-11-11 20:48:29 +0330

    #35 [Add] Create TermCourse model

--- university/models.py
+++ university/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -12,6 +12,19 @@ class Course(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     credits = models.FloatField(blank=False, null=False, validators=(MinValueValidator(0),))
     type = models.CharField(choices=COURSE_TYPES, blank=False, null=False)
-    pre_requisites = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True)
-    co_requisites = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True)
+    pre_requisites = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True
+                                       , related_name="post_courses")
+    co_requisites = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True
+                                      , related_name="co_courses")
     # school = models.ManyToManyField(School)
+
+
+class TermCourse(models.Model):
+    class_date_time = models.DateTimeField(blank=False, null=False)
+    exam_date_time = models.DateTimeField(blank=False, null=False)
+    exam_site = models.CharField(blank=False, null=False)
+    capacity = models.PositiveIntegerField(blank=False, null=False, validators=(MinValueValidator(250),))
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
+    # term = models.ForeignKey(Term, on_delete=models.CASCADE, blank=True, null=True)
+    # professor = models.ManyToManyField(Professor)
+
