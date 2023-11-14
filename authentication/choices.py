ca65df5c723b92f43c011eea6f15004c66ab274b
semjz <sam.malakjan@gmail.com>
2023-11-14 21:31:21 +0330

    [Add] Extract all choices from models Put choices in choices.py

---
+++ authentication/choices.py
@@ -0,0 +1,3 @@
+GENDERS_CHOICES = [("male", "male"), ("female", "female")]
+ROLES_CHOICES = [("student", "Student"), ("professor", "Professor"), ("assistant", "Assistant")
+    , ("it-manager", "It-manager")]
