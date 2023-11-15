04c1813a59290f705f39c4657e684e65245071a0
semjz <sam.malakjan@gmail.com>
2023-11-15 16:40:36 +0330

    Modify user role names

--- authentication/choices.py
+++ authentication/choices.py
@@ -1,3 +1,3 @@
 GENDERS_CHOICES = [("male", "male"), ("female", "female")]
-ROLES_CHOICES = [("student", "Student"), ("professor", "Professor"), ("assistant", "Assistant")
-    , ("it-manager", "It-manager")]
+ROLES_CHOICES = [("Student", "Student"), ("Professor", "Professor"), ("Assistant", "Assistant")
+    , ("IT Manager", "IT Manager")]
