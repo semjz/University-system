ffdd45f5597cb0bea84d128462b4f68daaf767bb
semjz <sam.malakjan@gmail.com>
2023-11-15 16:58:40 +0330

    #58 Check group instead of role

--- university/permissions.py
+++ university/permissions.py
@@ -4,4 +4,4 @@ from rest_framework import permissions
 class IsItManager(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.role == "it-manager"
+        return request.user.groups.filter(name="IT Manager")
