2defcde5b9b5afd319785dd5cebff55dd803b59a
semjz <sam.malakjan@gmail.com>
2023-11-15 17:23:11 +0330

    Remove TestView

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LogoutView, PasswordResetRequest, PasswordResetAction, TestView
+from .views import LogoutView, PasswordResetRequest, PasswordResetAction
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
