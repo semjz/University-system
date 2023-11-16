0beb44891439e509c38504332b68816d55b92866
semjz <sam.malakjan@gmail.com>
2023-11-16 15:05:06 +0330

    #73 Add Super Admin role in choices

--- authentication/choices.py
+++ authentication/choices.py
@@ -1,3 +1,3 @@
 GENDERS_CHOICES = [("male", "male"), ("female", "female")]
 ROLES_CHOICES = [("Student", "Student"), ("Professor", "Professor"), ("Assistant", "Assistant")
-                 , ("It Manager", "It Manager")]
+                 , ("It Manager", "It Manager"), ("Super Admin", "Super Admin")]
