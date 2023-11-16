2fc99b7f22d4b8ecd673cc0a4250288c058499bb
semjz <sam.malakjan@gmail.com>
2023-11-16 14:31:18 +0330

    #24 Superuser is separated from ITManager and creates its own user id

--- authentication/managers.py
+++ authentication/managers.py
@@ -1,35 +1,25 @@
 from django.contrib.auth.models import UserManager
-from django.utils.translation import gettext_lazy as g
 from django.contrib.auth.hashers import make_password
-
-import secrets
-import string
+from rolepermissions.roles import assign_role
 
 
 class CustomUserManger(UserManager):
     def create_user(self, user_id, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
+        is_staff = extra_fields.get("is_staff")
         email = self.normalize_email(email)
         role = extra_fields.get("role")
-        is_staff = extra_fields.get("is_staff")
         if not role and not is_staff:
             raise ValueError("The user role must be set")
         user = self.model(user_id=user_id, email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
+        assign_role(user, role)
         return user
 
     def create_superuser(self, user_id, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("role", "it-manager")
-        user_id = ''.join(secrets.choice(string.digits) for _ in range(4))
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(g("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(g("Superuser must have is_superuser=True."))
         self.create_user(user_id, email, password, **extra_fields)
-        print(f"user id: {user_id}")
