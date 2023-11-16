6470892c84bb8401102986d3a4ec586b17e18ad8
semjz <sam.malakjan@gmail.com>
2023-11-16 15:06:40 +0330

    #24 If user is admin no role is assigned

--- authentication/managers.py
+++ authentication/managers.py
@@ -10,16 +10,18 @@ class CustomUserManger(UserManager):
         is_staff = extra_fields.get("is_staff")
         email = self.normalize_email(email)
         role = extra_fields.get("role")
-        if not role and not is_staff:
+        if not role:
             raise ValueError("The user role must be set")
         user = self.model(user_id=user_id, email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
-        assign_role(user, role)
+        if not is_staff:
+            assign_role(user, role)
         return user
 
     def create_superuser(self, user_id, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", "Super Admin")
         self.create_user(user_id, email, password, **extra_fields)
