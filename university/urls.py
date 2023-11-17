2c4bbb8d814a1371054d3f8912d6425a8bf539a8
semjz <sam.malakjan@gmail.com>
2023-11-17 11:03:24 +0330

    #58 #59 Implement Crude with ITManagerStudentViewSet and add router for its urls

--- university/urls.py
+++ university/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import CreatStudent
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ITManagerStudentViewSet
 
 app_name = "university"
+router = DefaultRouter()
+router.register('admin/student', ITManagerStudentViewSet, basename="admin-student")
+
 urlpatterns = [
-    path("admin/students/", CreatStudent.as_view(), name="create-student")
+    path('', include(router.urls)),
 ]
--- university/views.py
+++ university/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .tasks import test_func
-from rest_framework.generics import CreateAPIView
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsItManager
 from .models import Student
@@ -11,7 +11,13 @@ def test(request):
     test_func.delay()
     return HttpResponse('Done')
 
-class CreatStudent(CreateAPIView):
+
+class ITManagerStudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     permission_classes = (IsAuthenticated, IsItManager)
     serializer_class = CreatStudentSerializer
+    http_method_names = ["get", "post", "put", "patch", "delete"]
+
+    def perform_destroy(self, instance):
+        instance.user.delete()
+
