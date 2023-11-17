9350bd3784d032a472c9f31311eb2a1f14358c5b
semjz <sam.malakjan@gmail.com>
2023-11-17 10:54:26 +0330

    #24 make blank first_name, last_name and email false

--- authentication/models.py
+++ authentication/models.py
@@ -6,16 +6,15 @@ from django.utils.translation import gettext_lazy as _
 from .choices import *
 
 
-# Create your models here.
 class User(AbstractUser):
 
     username = None
     user_id = models.CharField(max_length=12, validators=[MinLengthValidator(4)], unique=True
                                , blank=True, null=True)
 
-    first_name = models.CharField(_("first name"), max_length=150, blank=False)
-    last_name = models.CharField(_("last name"), max_length=150, blank=False)
-    email = models.EmailField(_("email address"), blank=False)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    email = models.EmailField(_("email address"))
 
     profile_image = models.ImageField("user_image")
     phone_number = models.CharField(_("phone number"), max_length=11, validators=[MinLengthValidator(11)]
@@ -30,6 +29,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "user_id"
     REQUIRED_FIELDS = ["email", "first_name", "last_name", "national_code", "phone_number"]
+
 
     def __str__(self):
         return str(self.username)
