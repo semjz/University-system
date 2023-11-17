d021eda3f81a1971988b96750b36de64ccfbc3e5
semjz <sam.malakjan@gmail.com>
2023-11-17 10:54:26 +0330

    #24 make blank first_name, last_name and email false

--- authentication/models.py
+++ authentication/models.py
@@ -5,7 +5,6 @@ from .managers import CustomUserManger
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
 class User(AbstractUser):
     GENDERS_CHOICES = [("male", "male"), ("female", "female")]
     ROLES_CHOICES = [("student", "Student"), ("professor", "Professor"), ("assistant", "Assistant")
@@ -15,9 +14,9 @@ class User(AbstractUser):
     user_id = models.CharField(max_length=8, validators=[MinLengthValidator(4)], unique=True
                                , blank=True, null=True)
 
-    first_name = models.CharField(_("first name"), max_length=150, blank=False)
-    last_name = models.CharField(_("last name"), max_length=150, blank=False)
-    email = models.EmailField(_("email address"), blank=False)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    email = models.EmailField(_("email address"))
 
     profile_image = models.ImageField("user_image")
     phone_number = models.CharField(_("phone number"), max_length=11, validators=[MinLengthValidator(11)]
@@ -32,6 +31,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "user_id"
     REQUIRED_FIELDS = ["email", "first_name", "last_name", "national_code", "phone_number", "role"]
+
 
     def __str__(self):
         return str(self.username)
