1b64b66293280b8f376f7bef27765c253f3c9ace
semjz <sam.malakjan@gmail.com>
2023-11-21 00:19:41 +0330

    #58 #59 Make sure BaseUserFactory phone_number is no longer than 14 digits

--- authentication/factories.py
+++ authentication/factories.py
@@ -15,6 +15,10 @@ def create_random_user_id():
     return ''.join(secrets.choice(string.digits) for _ in range(4))
 
 
+def coerce_phone_number(obj):
+    return obj.phone_number1.replace(" ", "")[:13]
+
+
 class BaseUserFactory(DjangoModelFactory):
     class Meta:
         model = User
@@ -26,7 +30,7 @@ class BaseUserFactory(DjangoModelFactory):
     last_name = factory.Faker('last_name')
     email = factory.Faker('email')
     phone_number1 = factory.Faker("phone_number", locale="fa_IR")
-    phone_number = factory.LazyAttribute(lambda o: o.phone_number1.replace(" ", ""))
+    phone_number = factory.LazyAttribute(coerce_phone_number)
     national_code = factory.LazyFunction(create_random_national_code)
     gender = factory.Iterator(["male", "female"])
     password = factory.Faker('password')
