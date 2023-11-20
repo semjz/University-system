7984781832dc6097203594ed7448e6064e8b8da6
semjz <sam.malakjan@gmail.com>
2023-11-20 21:44:05 +0330

    #59 Add appropriate lookup_expr and use correct fields in student filterset

--- university/filtersets.py
+++ university/filtersets.py
@@ -1,15 +1,17 @@
 from django_filters import rest_framework as filters
 from .models import Student
+from .choices import MILITARY_STATUS_CHOICES
 
 
 class StudentFilterSet(filters.FilterSet):
-    first_name = filters.CharFilter(field_name="user__first_name", label="First Name")
-    last_name = filters.CharFilter(field_name="user__last_name", label="Last Name")
+    first_name = filters.CharFilter(field_name="user__first_name", lookup_expr="icontains", label="First Name")
+    last_name = filters.CharFilter(field_name="user__last_name", lookup_expr="icontains", label="Last Name")
     national_code = filters.NumberFilter(field_name="user__national_code", label="National Code")
-    school = filters.NumberFilter(field_name="school", label="School")
+    school = filters.CharFilter(field_name="school", lookup_expr="icontains", label="School")
     entrance_year = filters.NumberFilter(field_name="entrance_year", label="Entrance Year")
-    military_status = filters.NumberFilter(field_name="military_status", label="Military Status")
-    major = filters.NumberFilter(field_name="major", label="Major")
+    military_status = filters.ChoiceFilter(choices=MILITARY_STATUS_CHOICES, field_name="military_status"
+                                           , label="Military Status")
+    major = filters.NumberFilter(field_name="major", lookup_expr="icontains", label="Major")
 
     class Meta:
         model = Student
