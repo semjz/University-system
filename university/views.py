501a6d75dacf2e048f4e12ada5d79604a35f6340
semjz <sam.malakjan@gmail.com>
2023-11-20 23:16:06 +0330

    #58 #59 Modify ITManagerStudentViewSet view to use UpdateStudentSerializer for put and patch

--- university/views.py
+++ university/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsItManager
 from .models import Student
-from .serializers import StudentSerializer
+from .serializers import CreateStudentSerializer, UpdateStudentSerializer
 from django_filters import rest_framework as filters
 import university.filtersets as filtersets
 
@@ -17,13 +17,18 @@ def test(request):
 class ITManagerStudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     permission_classes = (IsAuthenticated, IsItManager)
-    serializer_class = StudentSerializer
     http_method_names = ["get", "post", "put", "patch", "delete"]
     filter_backends = [filters.DjangoFilterBackend]
     filterset_class = filtersets.StudentFilterSet
 
     def perform_destroy(self, instance):
         instance.user.delete()
+
+    def get_serializer_class(self):
+        if self.action == "partial_update" or self.action == "update":
+            return UpdateStudentSerializer
+        else:
+            return CreateStudentSerializer
 
 
 
