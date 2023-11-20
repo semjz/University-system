161d01e54b1cae4d1d18f88c32a1127dbde91fca
semjz <sam.malakjan@gmail.com>
2023-11-20 21:39:25 +0330

    Modify IT Manger choice to match it's role

--- authentication/choices.py
+++ authentication/choices.py
@@ -1,3 +1,3 @@
 GENDERS_CHOICES = [("male", "male"), ("female", "female")]
 ROLES_CHOICES = [("Student", "Student"), ("Professor", "Professor"), ("Assistant", "Assistant")
-                 , ("It Manager", "It Manager"), ("Super Admin", "Super Admin")]
+                 , ("IT Manager", "It Manager"), ("Super Admin", "Super Admin")]
