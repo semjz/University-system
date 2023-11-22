4d250bc4744d95ea765c9397d2f945cc7503b4f8
semjz <sam.malakjan@gmail.com>
2023-11-22 05:59:28 +0330

    #69#70 refactor permission and use it in tests

--- university/permissions.py
+++ university/permissions.py
@@ -18,23 +18,35 @@ class IsAssistant(permissions.BasePermission):
         return has_role(request.user, Assistant)
 
 
-class IsITManagerOrIsAssistantForCourse(permissions.BasePermission):
+class IsITManagerOrIsCourseAssistant(permissions.BasePermission):
 
     def has_permission(self, request, view: viewsets):
-        is_schools_assistant = True
+        return has_role(request.user.user, ITManager) or (
+                    has_role(request.user.user, Assistant) and IsCourseAssistant())
+
+
+class IsCourseAssistant(permissions.BasePermission):
+
+    def has_permission(self, request, view):
         if view.action in ["update", "partial_update", "destroy"]:
-            course_id = view.kwargs["pk"]
-            schools = Course.objects.get(id=course_id).schools.all()
-
-            for school in schools:
-                if request.user.user_id != school.assistant.user_id:
-                    is_schools_assistant = False
+            is_schools_assistant = self.has_permission_for_update(request, view)
         else:
-            school_ids = request.data["schools"]
-            for school_id in school_ids:
-                school = School.objects.get(id=school_id)
-                if request.user.user_id != school.assistant.user_id:
-                    is_schools_assistant = False
+            is_schools_assistant = self.has_permission_for_create(request)
+        return is_schools_assistant
 
-        return (has_role(request.user.user, ITManager) or
-                (has_role(request.user.user, Assistant) and is_schools_assistant))
+    def has_permission_for_create(self, request):
+        school_ids = request.data["schools"]
+        for school_id in school_ids:
+            school = School.objects.get(id=school_id)
+            if request.user.user_id != school.assistant.user_id:
+                return False
+        return True
+
+    def has_permission_for_update(self, request, view):
+        course_id = view.kwargs["pk"]
+        schools = Course.objects.get(id=course_id).schools.all()
+
+        for school in schools:
+            if request.user.user_id != school.assistant.user_id:
+                return False
+        return True
