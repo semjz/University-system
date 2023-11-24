60bcd02c85770068e74b9eba3aa53ad37aaa10d0
semjz <sam.malakjan@gmail.com>
2023-11-24 12:50:56 +0330

    feat: CreateUserSerializer inherits form UpdateUserSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -14,44 +14,6 @@ class AssistantSerializer(serializers.ModelSerializer):
         model = Assistant
         fields = '__all__'
 
-class CreateUserSerializer(serializers.ModelSerializer):
-    confirm_password = serializers.CharField(max_length=128, required=True, write_only=True)
-
-    class Meta:
-        model = User
-        fields = ["user_id", "first_name", "last_name", "national_code", "phone_number", "email"
-            , "role", "gender", "birth_date", "password", "confirm_password"]
-        read_only_fields = ["user_id"]
-
-    def validate_password(self, password):
-        validate_password(password)
-        return password
-
-    def validate_national_code(self, national_code):
-        if not national_code.isnumeric():
-            raise serializers.ValidationError("National code must only contain digits!")
-        return national_code
-
-    def validate_phone_number(self, phone_number):
-        if "+" in phone_number:
-            phone_number = phone_number.replace("+", "")
-        if not phone_number.isnumeric():
-            raise serializers.ValidationError("Phone number code must only contain digits!")
-        return phone_number
-
-    def validate(self, data):
-        if data["password"] != data["confirm_password"]:
-            raise serializers.ValidationError("Passwords don't match!")
-        return data
-
-    def create(self, validated_data):
-        validated_data.pop("confirm_password")
-        user = User.objects.create_user(
-            **validated_data
-        )
-        return user
-
-
 class UpdateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -73,6 +35,32 @@ class UpdateUserSerializer(serializers.ModelSerializer):
         for field, value in validated_data.items():
             setattr(instance, field, value)
         return instance
+
+
+class CreateUserSerializer(UpdateUserSerializer):
+    confirm_password = serializers.CharField(max_length=128, required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["user_id", "first_name", "last_name", "national_code", "phone_number", "email"
+                   , "role", "gender", "birth_date", "password", "confirm_password"]
+        read_only_fields = ["user_id"]
+
+    def validate_password(self, password):
+        validate_password(password)
+        return password
+
+    def validate(self, data):
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError("Passwords don't match!")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("confirm_password")
+        user = User.objects.create_user(
+            **validated_data
+        )
+        return user
 
 
 class ChangePasswordSerializer(serializers.ModelSerializer):
