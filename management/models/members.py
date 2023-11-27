abca8b137a41ef9efd0a8f4e64ff43d5aad9c98e
semjz <sam.malakjan@gmail.com>
2023-11-27 20:55:43 +0330

    modify: remove user_id permission and post_signal from model

--- management/models/members.py
+++ management/models/members.py
@@ -1,30 +1,21 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models import Avg, Count
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from guardian.shortcuts import assign_perm
-
-from authentication.roles import AssistantRole
-from management.mixins import UserIdPermissionMixin
 from utils.choices import *
 
 
 User = get_user_model()
 
 
-class ITManager(UserIdPermissionMixin, models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class ITManager(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user_id
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
     supervisor = models.ForeignKey(to="Professor", on_delete=models.CASCADE, null=True, blank=True
                                    , related_name="student")
     major = models.ForeignKey(to="management.Major", on_delete=models.CASCADE, related_name="students")
@@ -49,7 +40,7 @@ class Student(models.Model):
 
 
 class Professor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
     school = models.ForeignKey(to="management.Faculty", on_delete=models.CASCADE, related_name="professors")
     past_courses = models.ManyToManyField(to="academic.Course", blank=True)
     major = models.ForeignKey(to="management.Major", on_delete=models.CASCADE, related_name="professors")
@@ -57,19 +48,10 @@ class Professor(models.Model):
     rank = models.CharField(choices=PROFESSOR_RANK_CHOICES, max_length=20, null=True, blank=True)
 
 
-class Assistant(UserIdPermissionMixin, models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Assistant(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
     school = models.OneToOneField(to="management.Faculty", on_delete=models.CASCADE)
     major = models.OneToOneField(to="management.Major", on_delete=models.CASCADE)
 
 
-@receiver(post_save, sender=Assistant)
-def user_post_save(sender, **kwargs):
-    """
-    Create a Profile instance for all newly created User instances. We only
-    run on user creation to avoid having to check for existence on each call
-    to User.save.
-    """
-    assistant, created = kwargs["instance"], kwargs["created"]
-    if created and assistant.user_id != settings.ANONYMOUS_USER_NAME:
-        assign_perm("management.can_modify_user_id", assistant.user)
+
