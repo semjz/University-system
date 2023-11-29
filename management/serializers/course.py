748fbdf3e43ec1ecb1f4c9e62b06ed71e64f08e3
semjz <sam.malakjan@gmail.com>
2023-11-29 16:42:35 +0330

    Delete CourseSerializer in management app

--- management/serializers/course.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from academic.models import Course
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        exclude = ("id",)
