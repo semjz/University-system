e27ba6d2b405ae17f7fc6271530c33e9abd0204f
semjz <sam.malakjan@gmail.com>
2023-11-29 21:52:41 +0330

    Create ChangePasswordRequestSerializer and modify ChangePasswordActionSerializer move checking if reset token exists from serializer to view

--- authentication/serializers/auth.py
+++ authentication/serializers/auth.py
@@ -1,44 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from django.core.cache import cache
+
 
 User = get_user_model()
 
 
-class ChangePasswordSerializer(serializers.ModelSerializer):
-    new_pass = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    confirm_new_pass = serializers.CharField(write_only=True, required=True)
-    old_pass = serializers.CharField(write_only=True, required=True)
-    reset_token = serializers.CharField(write_only=True, max_length=50)
+class ChangePasswordRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
 
-    class Meta:
-        model = User
-        fields = ["old_pass", "new_pass", "confirm_new_pass", "reset_token"]
 
-    def validate_old_pass(self, password):
-        user = self.context['request'].user
-        if not user.check_password(password):
-            raise serializers.ValidationError("Old password is not correct!")
-        return password
+class ChangePasswordActionSerializer(serializers.Serializer):
+    new_pass = serializers.CharField(required=True, validators=[validate_password])
+    confirm_new_pass = serializers.CharField(required=True)
+    reset_token = serializers.CharField(max_length=50, required=True)
 
     def validate_reset_token(self, reset_token):
-        user = self.context['request'].user
-        if not cache.get(user.user_id):
-            raise serializers.ValidationError("No password reset request submitted for this user")
-        if not str(cache.get(user.user_id)) == reset_token:
-            raise serializers.ValidationError("Reset token is not correct!")
+        if not reset_token:
+            serializers.ValidationError("A reset token must be provided!")
+        return reset_token
 
     def validate(self, data):
         if data['new_pass'] != data['confirm_new_pass']:
             raise serializers.ValidationError("Passwords don't match!")
 
         return data
-
-    def update(self, instance, validated_data):
-        instance.set_password(validated_data['new_pass'])
-        instance.save()
-        return instance
 
 
 class LogoutSerializer(serializers.Serializer):
