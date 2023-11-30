6d272b8b4b0f999dc5f737aa792a2b438fb993da
semjz <sam.malakjan@gmail.com>
2023-11-30 14:06:48 +0330

    Add admin faculty urls

--- management/serializers/__init__.py
+++ management/serializers/__init__.py
@@ -1,1 +1,4 @@
 from .student import CreateStudentSerializer, RUDStudentSerializer
+from .term import TermSerializer
+from .professor import ProfessorSerializer
+from .Faculty import FacultySerializer
