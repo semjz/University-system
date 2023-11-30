e7c1045b33105a9b7b52750283833a288dcbff82
ali <alikhodayariard4484@gmail.com>
2023-11-30 20:35:11 +0330

    feat: Implement Professors urls

--- academic/urls.py
+++ academic/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import (CourseViewSet, TermCourseViewSet, StudentListRetrieveSet, StudentAllowedCoursesView
-, StudentRemainingTermsView, StudentCoursesReportView)
+, StudentRemainingTermsView, StudentCoursesReportView,ProfessorListRetrieveSet)
 
 app_name = "academic"
 router = DefaultRouter()
 router.register('subjects', CourseViewSet, basename="subjects")
 router.register('courses', TermCourseViewSet, basename="courses")
 router.register('students', StudentListRetrieveSet, basename="students")
+router.register('professors', ProfessorListRetrieveSet , basename="professors")
 
 urlpatterns = [
     path('', include(router.urls)),
